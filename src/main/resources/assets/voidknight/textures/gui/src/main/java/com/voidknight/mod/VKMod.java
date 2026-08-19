9e2449e9c2d0236738084354dea5f1ea47811dde
YashTheGreat <qezrathegreat@gmail.com>
2026-08-19 14:33:57 +0530

    Update VKMod.java

--- src/main/resources/assets/voidknight/textures/gui/src/main/java/com/voidknight/mod/VKMod.java
+++ src/main/resources/assets/voidknight/textures/gui/src/main/java/com/voidknight/mod/VKMod.java
@@ -5,7 +5,5 @@ import net.fabricmc.api.ClientModInitializer;
 public class VKMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Auto-sync start
-        DataManager.startAutoSync();
     }
 }
