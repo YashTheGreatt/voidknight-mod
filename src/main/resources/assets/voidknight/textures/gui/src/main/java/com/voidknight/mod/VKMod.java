69f7b239f1c365a69aad40ea75b7c91d12eb7421
YashTheGreat <qezrathegreat@gmail.com>
2026-08-19 15:10:45 +0530

    Update VKMod.java

--- src/main/resources/assets/voidknight/textures/gui/src/main/java/com/voidknight/mod/VKMod.java
+++ src/main/resources/assets/voidknight/textures/gui/src/main/java/com/voidknight/mod/VKMod.java
@@ -5,5 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class VKMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        System.out.println("[VoidKnight] Mod loaded successfully!");
     }
 }
