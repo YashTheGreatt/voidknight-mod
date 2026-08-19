0443efb865486d7afee2cb5358779ff2db48f0d2
YashTheGreat <qezrathegreat@gmail.com>
2026-08-19 20:13:41 +0530

    Update VKMod.java

--- src/main/java/com/voidknight/mod/VKMod.java
+++ src/main/java/com/voidknight/mod/VKMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -13,17 +12,13 @@ public class VKMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        try {
-            ModContainer container = FabricLoader.getInstance().getModContainer(MOD_ID).orElse(null);
-            if (container != null) {
-                ResourceManagerHelper.registerBuiltinResourcePack(
-                    Identifier.of(MOD_ID, "voidknight_resources"),
-                    container,
-                    Text.literal("VoidKnight Clan Resources"),
-                    ResourcePackActivationType.ALWAYS_ENABLED
-                );
-            }
-        } catch (Throwable ignored) {
-        }
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(container -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(
+                Identifier.of(MOD_ID, "voidknight_resources"),
+                container,
+                Text.literal("VoidKnight Clan Resources"),
+                ResourcePackActivationType.ALWAYS_ENABLED
+            );
+        });
     }
 }
