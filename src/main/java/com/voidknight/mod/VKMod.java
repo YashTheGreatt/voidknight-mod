481e57b5c61c9bc6afd4dc9a7e4c687f26ae8ff5
YashTheGreat <qezrathegreat@gmail.com>
2026-08-20 21:46:21 +0530

    Update VKMod.java

--- src/main/java/com/voidknight/mod/VKMod.java
+++ src/main/java/com/voidknight/mod/VKMod.java
@@ -4,10 +4,8 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class VKMod implements ClientModInitializer {
 
-    public static final String MOD_ID = "voidknight";
-
     @Override
     public void onInitializeClient() {
-        System.out.println("[VoidKnight] Loaded on Minecraft 1.21.11");
+        System.out.println("[VoidKnight] VoidKnight loaded successfully!");
     }
 }
