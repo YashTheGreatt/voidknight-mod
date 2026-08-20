2687eb8a690fc0c04792e1cc0b3ab660fecc0c07
YashTheGreat <qezrathegreat@gmail.com>
2026-08-20 18:04:21 +0530

    Update MemberInfo.java

--- src/main/java/com/voidknight/mod/MemberInfo.java
+++ src/main/java/com/voidknight/mod/MemberInfo.java
@@ -2,7 +2,6 @@ package com.voidknight.mod;
 
 public class MemberInfo {
     public String mode;
-    public String tier;
-    public String type;
     public String role;
+    public int points;
 }
