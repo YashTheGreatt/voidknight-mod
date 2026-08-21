4237c79612293b63236325bde019b3c6b6e29813
YashTheGreat <qezrathegreat@gmail.com>
2026-08-21 16:31:26 +0530

    Update EntityRendererMixin.java

--- src/main/java/com/voidknight/mod/mixin/EntityRendererMixin.java
+++ src/main/java/com/voidknight/mod/mixin/EntityRendererMixin.java
@@ -1,8 +1,9 @@
 package com.voidknight.mod.mixin;
 
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.renderer.entity.player.AvatarRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(PlayerEntityRenderer.class)
+@Mixin(AvatarRenderer.class)
 public abstract class EntityRendererMixin {
+
 }
